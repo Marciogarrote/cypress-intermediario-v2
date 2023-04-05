ee2691f63a8498eac3974f57254d0793ad753d16
MarcioGarrote <marciosantosgarrote@gmail.com>
2023-04-04 23:04:03 -0300

    Curso intermediario Cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress') //primeiro fazer a configuração do require, da função defineConfig que vai definir a configuração do projeto de testes
+
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost',
+  },
+  fixturesFolder: false, 
+  video: false,
+})
