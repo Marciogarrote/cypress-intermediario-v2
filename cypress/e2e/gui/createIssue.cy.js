301af912d1cea5ca99852237554fd7dcd5f51f17
MarcioGarrote <marciosantosgarrote@gmail.com>
2023-04-18 22:17:30 -0300

    new file:   cypress/cypress.config.js 	new file:   cypress/cypress/fixtures/example.json 	new file:   cypress/cypress/support/commands.js 	new file:   cypress/cypress/support/e2e.js 	new file:   cypress/e2e/api/createProject.cy.js 	new file:   cypress/e2e/gui/createIssue.cy.js 	modified:   cypress/e2e/gui/login.cy.js 	new file:   cypress/support/api_commands.js 	modified:   cypress/support/e2e.js 	modified:   cypress/support/gui_commands.js

---
+++ cypress/e2e/gui/createIssue.cy.js
@@ -0,0 +1,33 @@
+import { faker } from '@faker-js/faker'
+
+//Criando nosso describe para criar a issue com sucesso
+// criamos um objeto que definimos numa variavel chamada issue
+
+describe('Create Issue', () => {
+  const issue = {
+    title: `issue-${faker.datatype.uuid()}`,
+    description: faker.random.words(3),
+    project: {
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5)
+    }
+  }
+
+    // beforeEach definimos a nossa pré condição
+    // tem que estar logado e tem que existir um projeto
+  beforeEach(() => {
+    cy.login()
+    cy.gui_createProject(issue.project)
+  })
+
+   //Ação
+  //nossa ação que recebe a issue do objeto acima, e depois verificamos o resultado esperado
+  //um elemento que contem issue details tem que ter a issue title e issue description
+  it('successfully', () => {
+    cy.gui_createIssue(issue)
+    //Resultado esperado
+    cy.get('.issue-details')
+      .should('contain', issue.title)
+      .and('contain', issue.description)
+  })
+})
